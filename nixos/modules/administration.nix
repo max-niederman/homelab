7da2bfddf4bb77a54f3ccf56303bd73afcffacb3
Max Niederman <max@maxniederman.com>
2021-11-22 20:06:55 -0800

    fix(administration): add tar-amandil ssh key

--- nixos/modules/administration.nix
+++ nixos/modules/administration.nix
@@ -13,6 +13,7 @@
         (_: {
           openssh.authorizedKeys.keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmdKg6WzEiyKysklc3YAKLjHEDLZq4RAjRYlSVbwHs9 max@tar-minyatur"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDF+G95MjnRRGGGutAj7Zs78spNvZzWu0CagHUEE8LmR max@tar-amandil"
           ];
         });
     };
