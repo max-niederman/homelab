cc9ec6624dcfa865828db6fb20f31d8298e2eec8
Max Niederman <max@maxniederman.com>
2022-04-13 13:49:27 -0700

    feat(shell): add deployment helper wrapping nixops and ssh

--- shell.nix
+++ shell.nix
@@ -4,13 +4,29 @@
 
 with lib;
 let
-  hosts = import ./servers/hosts { };
+  hosts = import ./hosts { };
   server = hosts.withName "beleg";
+
+  homelab-deploy =
+    let
+      leaderAddr = (head (hosts.withRole "leader")).address;
+    in
+    pkgs.writeShellScriptBin "homelab-deploy" ''
+      nixops deploy
+
+      if [ -z $1 ]; then
+        ssh ${leaderAddr} -t "homelab-deploy-all"
+      else
+        ssh ${leaderAddr} -t "homelab-deploy $1"
+      fi
+    '';
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     docker
     docker-compose
     nixpkgs-fmt
+
+    homelab-deploy
   ];
 }
