4a52fdfb3cf5d9b6a86169e2dc069d27c9193fdb
Max Niederman <max@maxniederman.com>
2024-05-17 21:40:42 -0700

    hosts/beleg: update hardware config

--- hosts/beleg/hardware-configuration.nix
+++ hosts/beleg/hardware-configuration.nix
@@ -18,12 +18,6 @@
       fsType = "zfs";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1F8E-6BBC";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
-
   fileSystems."/nix" =
     { device = "rpool/local/nix";
       fsType = "zfs";
@@ -34,9 +28,20 @@
       fsType = "zfs";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/b4f8fb02-c901-4a5b-81d8-1466dc7b487f"; }
-    ];
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/7ADD-4706";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
