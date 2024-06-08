2357a90f7de208bfde5c76306fe06eabb5a4ffec
Max Niederman <max@maxniederman.com>
2024-06-07 21:48:32 -0700

    feat(monitoring): add loki and grafana

--- hosts/beleg/services/monitoring.nix
+++ hosts/beleg/services/monitoring.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config,...}: {
   config = {
     services.prometheus = {
       enable = true;
@@ -23,13 +23,72 @@
       ];
     };
 
+    services.loki = {
+      enable = true;
+      configuration = {
+        auth_enabled = false;
+
+        analytics.reporting_enabled = false;
+
+        server = {
+          http_listen_port = 3100;
+          grpc_listen_port = 0;
+        };
+
+        common = {
+          instance_addr = "127.0.0.1";
+          replication_factor = 1;
+
+          ring.kvstore.store = "inmemory";
+
+          path_prefix = config.services.loki.dataDir;
+          storage.filesystem = {
+            chunks_directory = "${config.services.loki.dataDir}/chunks";
+            rules_directory = "${config.services.loki.dataDir}/rules";
+          };
+        };
+
+        schema_config.configs = [
+          {
+            from = "2024-06-01";
+            store = "tsdb";
+            object_store = "filesystem";
+            schema = "v13";
+            index = {
+              prefix = "index_";
+              period = "24h";
+            };
+          }
+        ];
+      };
+    };
+
+    services.grafana = {
+      enable = true;
+      settings = {
+        server = {
+          http_addr = "192.168.0.11";
+          http_port = 3000;
+        };
+        analytics.reporting_enabled = false;
+      };
+    };
+
     systemd.tmpfiles.rules = [
-      "L /var/lib/prometheus2 - - - - /persist/var/lib/prometheus2"
+      "L /var/lib/prometheus2 - prometheus - - /persist/var/lib/prometheus2"
+      "L /var/lib/loki        - loki       - - /persist/var/lib/loki"
+      "L /var/lib/grafana     - grafana    - - /persist/var/lib/grafana"
     ];
 
     services.caddy.virtualHosts = {
       "prometheus.maximal.enterprises".extraConfig = ''
         reverse_proxy 192.168.0.11:9090
+      '';
+      "loki.maximal.enterprises".extraConfig = ''
+        reverse_proxy 192.168.0.11:3100
+      '';
+      "grafana.maximal.enterprises".extraConfig = ''
+        reverse_proxy 192.168.0.11:3000
       '';
     };
   };
