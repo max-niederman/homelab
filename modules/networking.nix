cf9127e47fa628cd4f0dcc017c3a7a6c2312ff51
Max Niederman <max@maxniederman.com>
2024-06-07 18:02:26 -0700

    feat: setup TLS with Cloudflare DNS

--- modules/networking.nix
+++ modules/networking.nix
@@ -4,7 +4,12 @@
       firewall.enable = false;
 
       useDHCP = false;
-      nameservers = ["192.168.0.2"];
+      nameservers = [
+        "1.1.1.1"
+        "1.0.0.1"
+        "2606:4700:4700::1111"
+        "2606:4700:4700::1001"
+      ];
     };
 
     systemd.network = {
